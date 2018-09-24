4d11d432c928fbfb355313cf856e720bac999f29
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2018-09-24 18:43:16 -0500

    setup angular app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'myapp'`, async(() => {
+  it(`should have as title 'myApp'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('myapp');
+    expect(app.title).toEqual('myApp');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to myapp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to myApp!');
   }));
 });
