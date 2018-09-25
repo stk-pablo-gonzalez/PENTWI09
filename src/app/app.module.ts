8c4d32ec12ce0f1b5e59c1e492ccb0ba07db67b5
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2018-09-24 19:02:49 -0500

    practices with angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FoodListComponent } from './foods/food-list/food-list.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodListComponent,
+    MyComponentComponent
   ],
   imports: [
     BrowserModule
